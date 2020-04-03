4c88af674a525752c50fb95aba68362c64d55dd2
Thomson <rentongxin@sjtu.edu.cn>
2020-04-03 20:37:13 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -167,11 +167,23 @@ texinfo_documents = [
 
 
 latex_engine = 'xelatex'
-latex_use_xindy = False
 latex_elements = {
-    'preamble': '\\usepackage[UTF8]{ctex}\n',
-    passoptionstopackages: '\\PassOptionsToPackage{warn}{textcomp}',
+    'fontpkg': r'''
+\setmainfont{DejaVu Serif}
+\setsansfont{DejaVu Sans}
+\setmonofont{DejaVu Sans Mono}
+''',
+    'preamble': r'''
+\usepackage[titles]{tocloft}
+\cftsetpnumwidth {1.25cm}\cftsetrmarg{1.5cm}
+\setlength{\cftchapnumwidth}{0.75cm}
+\setlength{\cftsecindent}{\cftchapnumwidth}
+\setlength{\cftsecnumwidth}{1.25cm}
+''',
+    'fncychap': r'\usepackage[Bjornstrup]{fncychap}',
+    'printindex': r'\footnotesize\raggedright\printindex',
 }
+latex_show_urls = 'footnote'
 
 
 
